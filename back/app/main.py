8563c9fc59d7b2ac95e829e55edada4047ff57be
seobin <qkdtjqls@naver.com>
2026-03-06 14:29:33 +0900

    API update

--- back/app/main.py
+++ back/app/main.py
@@ -1,20 +1,28 @@
+from __future__ import annotations
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import predict 
+from app.api.predict import router as predict_router
 
-app = FastAPI()
+app = FastAPI(title="cre-morph-cls API", version="0.1.0")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.2:5173",
+]
 
 #  React 프론트엔드에서 API 호출 시 CORS 문제를 방지하기 위해 CORS 미들웨어 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],       # 어떤 곳에서 오는 요청을 허용할지 / 개발 단계에서는 "*"로 모든 도메인을 허용 실제 배포시엔 특정 주소만 허용
+    allow_origins=origins,       # 어떤 곳에서 오는 요청을 허용할지 / 개발 단계에서는 "*"로 모든 도메인을 허용 실제 배포시엔 특정 주소만 허용
     allow_credentials=True,     # 쿠키나 인증 정보를 포함할지
     allow_methods=["*"],         # GET, POST 등 어떤 메소드를 허용할지
     allow_headers=["*"],         # 어떤 HTTP 헤더를 허용할지
 )
 
 # predict 관련 API를 /predict 경로로 라우팅
-app.include_router(predict.router, prefix="/predict")
+app.include_router(predict_router)
+
 
 @app.get("/")
 def root():
